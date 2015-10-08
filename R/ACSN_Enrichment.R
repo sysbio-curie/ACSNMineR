a4b0f26eba7bb1f84638e36c7d32a4c05b320d76
DeveauP <paul.deveau@polytechnique.edu>
2015-10-08 11:22:51 +0200

    Committing first files
    Format files and structure

---
+++ R/ACSN_Enrichment.R
@@ -0,0 +1,61 @@
+####### ACSN enrichment analysis
+
+enrichment<-function(Genes=NULL,
+                    maps = list(ACSN_apop_formatted,
+                                ACSN_cellcyc_formatted,
+                                ACSN_dnarep_formatted,
+                                ACSN_emtmotil_formatted,
+                                ACSN_master_formatted,
+                                ACSN_survival_formatted), 
+                    correction_multitest = FALSE,
+                    statistical_test = "fisher",
+                    min_module_size = 5,
+                    universe = "HUGO"){
+  if(universe == "HUGO"){
+    ###Total size of approved symbols, from http://www.genenames.org/cgi-bin/statistics, as of October 8th 2015
+    size = 39480
+  }
+  else if(universe == "ACSN"){
+    genes<-character()
+    for(lt in maps){
+      ### extract modules of size >= module_size
+      
+      genes<-unique(lapply(X = maps, FUN = function(z){z[,-(1:2)]}))
+      genes<-genes[genes != ""]
+      size <- length(genes)
+      print(size)
+    }
+    
+  }
+  else if (is.numeric(universe)){
+    size = universe
+  }
+  else{
+    stop("Invalid universe input: must be 'HUGO','ACSN', or numeric")
+  }
+  ### get from list how many are in each sub-compartment
+  result<-data.frame()
+  ### what would be the expected?
+  for(map in maps){
+    modules<-map[,1]
+    if(statistical_test == "fisher"){
+      
+    }
+  }
+}
+
+represent_enrichment<-function(enrichment){
+  
+}
+
+format_from_gmt<-function(path = ""){
+  gmt<-read.csv(path,header = FALSE, sep = "\t")
+  result<-t(apply(gmt,1,FUN = function(z){
+          pos<-grep(pattern = "\\*",x = z)
+          res<-z
+          res[pos]<-""
+          return(res)
+          }))
+  result[,2]<-apply(result[,-(1:2)], 1, FUN = function(z) sum(z!=""))
+  return(result)
+}
